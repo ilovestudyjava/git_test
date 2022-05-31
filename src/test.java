4ca4b7c41c71585a56921f853e2923b07d62b091
lemon <2358149165@qq.com>
2022-06-01 01:45:03 +0800

    test first commit

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author : 柠檬呀
+ * @version ：1.0
+ * @date : 2022/6/1 01:1:41
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}
